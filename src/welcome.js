13d94f3863146230729c1d9f8224277f3af2db6f
michaelmalonenz <michael.j.b.malone@gmail.com>
2016-04-03 19:47:16 +1200

    Snapshot of the code used to propose

--- src/welcome.js
+++ src/welcome.js
@@ -7,6 +7,8 @@ export class Welcome {
   constructor() {
     this.dragDirection = DIRECTION.HORIZONTAL;
     this.reset();
+    this.questionLetters = [];
+    this._gatherStringToArray(this.questionLetters, "Will you marry me?");
   }
 
   get unjumbledName() {
@@ -15,9 +17,7 @@ export class Welcome {
 
   submit() {
     this.nameLetters = [];
-    for(let i = 0; i < this.firstName.length; i++) {
-      this.nameLetters.push(new Letter(this.firstName.charAt(i)));
-    }
+    this._gatherStringToArray(this.nameLetters, this.firstName);
 
     do {
       ArrayExtensions.shuffle(this.nameLetters);
@@ -32,7 +32,9 @@ export class Welcome {
     let itemId = item.dataset.id;
     let siblingId = sibling ? sibling.dataset.id : null;
     moveBefore(this.nameLetters, (letter) => letter.id === itemId, (letter) => letter.id === siblingId);
-    if (this.unjumbledName === this.firstName)
+    if (this.unjumbledName === 'Farah')
+      this.farahWinner = true;
+    else if (this.unjumbledName === this.firstName)
       this.winner = true;
   }
 
@@ -40,5 +42,11 @@ export class Welcome {
     this.firstName = '';
     this.haveName = false;
     this.winner = false;
+  }
+
+  _gatherStringToArray(array, string) {
+     for(let i = 0; i < string.length; i++) {
+      array.push(new Letter(string.charAt(i)));
+    }
   }
 }
