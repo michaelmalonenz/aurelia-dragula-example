195662c9619ea5e3b4b4d7ffcb3bd22dd4581b8f
Michael Malone <michael.j.b.malone@gmail.com>
2016-04-27 19:42:06 +1200

    building

--- config.js
+++ config.js
@@ -6,7 +6,6 @@ System.config({
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*"
   },
-
   map: {
     "aurelia-animator-css": "npm:aurelia-animator-css@1.0.0-beta.1.2.0",
     "aurelia-binding": "npm:aurelia-binding@1.0.0-beta.1.3.2",
